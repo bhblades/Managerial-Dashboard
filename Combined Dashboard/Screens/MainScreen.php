cb267dde6bdfb3812d1c2531abd40dc6a7a9f782
Juliana Merlino <jfmerlino@loyola.edu>
2024-03-21 11:07:52 -0400

    Finally got out server working! Bolden is creating Azure DevOps connection but for now we are hard coding data to make sure php code works and front end looks nice.

---
+++ Combined Dashboard/Screens/MainScreen.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Combined Dashboard</title>
+</head>
+<body>
+<form method="post" action="process.php">
+    <button type="submit" name="button1">Button 1</button>
+    <button type="submit" name="button2">Button 2</button>
+    <button type="submit" name="button3">Button 3</button>
+    <button type="submit" name="button4">Button 4</button>
+    <button type="submit" name="button5">Button 5</button>
+    <button type="submit" name="button6">Button 6</button>
+</form>
+</body>
+</html>
---
+++ Combined Dashboard/Screens/MetricScreen.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Combined Dashboard</title>
+</head>
+<body>
+<form method="post" action="process.php">
+    <button type="submit" name="button1">Progress</button>
+    <button type="submit" name="button2">Cycle Time</button>
+    <button type="submit" name="button3">Lead Time</button>
+    <button type="submit" name="button4">Processing Time</button>
+    <button type="submit" name="button5">Waiting Time</button>
+    <button type="submit" name="button6">Bugs</button>
+</form>
+</body>
+</html>
